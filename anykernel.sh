e548f0f6a2712a0b1b65f36e94761c4e917cd538
cumaRull <rulsmods@gmail.com>
2023-08-15 07:02:25 +0700

    anykernel

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Aosp for realme C25 Series
+kernel.string=AnyKernel3 for realme C25 Series
 do.devicecheck=1
 do.modules=0
 do.systemless=1
